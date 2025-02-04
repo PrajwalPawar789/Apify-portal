9fba796c4d77e36e47eff66ffea03e5cd406cf62
Prajwal Pawar <114561900+PrajwalPawar789@users.noreply.github.com>
2025-02-04 05:38:04 +0530

    Delete middleware directory

--- middleware/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-export const ensureAuthenticated = (req, res, next) => {
-    if (req.session && req.session.user) {
-        // User is logged in
-        next();
-    } else {
-        // Redirect to login page if not authenticated
-        res.redirect('/login');
-    }
-};
